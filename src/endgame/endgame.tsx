4c4030ebfa199aa9f762b0a89f2eee59ea4e0de4
Kacegz <90635502+Kacegz@users.noreply.github.com>
2024-06-30 22:09:17 +0200

    update endgame modal

--- src/endgame/endgame.tsx
+++ src/endgame/endgame.tsx
@@ -1,4 +1,5 @@
 import { format } from "date-fns";
+import styles from "./endgame.module.scss";
 interface Params {
   time: number;
   startTime: number;
@@ -12,20 +13,23 @@ export default function Endgame({
   setNickname,
 }: Params) {
   return (
-    <div>
-      <h1>Arr! Congrats!</h1>
-      <h2>Your time:</h2>
-      <h1>{format(time - startTime, "m.ss.SS")}</h1>
-      <h2>Enter your nickname</h2>
+    <>
+      <h1>Congratulations!</h1>
+      <p>You found all characters</p>
+      <h3>Your time:</h3>
+      <h3>{format(time - startTime, "m.ss.SS")} seconds</h3>
+      <p>Enter your nickname:</p>
       <form onSubmit={saveUser} method="post">
-        <input
-          type="text"
-          name="nickname"
-          id="nickname"
-          onChange={(e) => setNickname(e.target.value)}
-        />
-        <input type="submit" value="Save" />
+        <div className={styles.flex}>
+          <input
+            type="text"
+            name="nickname"
+            id="nickname"
+            onChange={(e) => setNickname(e.target.value)}
+          />
+          <input type="submit" value="Save" />
+        </div>
       </form>
-    </div>
+    </>
   );
 }
