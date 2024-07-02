ab9211fccab906a68902f5a08192a1a605e57c27
Kacegz <90635502+Kacegz@users.noreply.github.com>
2024-07-02 22:49:32 +0200

    Change styles and add warning for mobile devices

--- src/start/Start.tsx
+++ src/start/Start.tsx
@@ -1,5 +1,6 @@
 import loadingImg from "../assets/parrot.png";
 import styles from "./start.module.scss";
+import { isMobile } from "react-device-detect";
 export default function Start({
   setOpenStart,
   startGame,
@@ -11,6 +12,9 @@ export default function Start({
     <>
       <img src={loadingImg} className={styles.parrot} alt="" />
       <h1>Welcome to Where's Pirate!</h1>
+      {isMobile && (
+        <h3 style={{ color: "red" }}>WARNING: Mobile devices unsupported</h3>
+      )}
       <p>Prepare for a Swashbuckling Adventure!</p>
       <p>Your mission is to locate and tag all 4 characters.</p>
       <p>Stay sharp and be quick, as every second counts in this hunt!</p>
